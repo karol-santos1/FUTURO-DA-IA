d655d9ba68b0cc64818be7931f00a3644541215c
karoline silveira dos santos <111357509+karol-santos1@users.noreply.github.com>
2024-06-13 09:21:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixapergunta = document.querySelector(".caixa-pergunta");
+const caixaalternativas = document.querySelector(".caixa-alternativa
+const caixaresultado = document.querySelector(".caixa-resultado");
